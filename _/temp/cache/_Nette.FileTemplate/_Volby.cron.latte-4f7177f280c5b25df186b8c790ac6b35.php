ef7c9524667558b7e194e3e02128d52ecb880ea9
LS <l.sevcuj@gmail.com>
2014-10-08 02:05:53 +0200

    Revert "Senat a komunálky 2014"
    This reverts commit 409cc6f94991e3476dd787afbfded9b33ac534d5.

--- _/temp/cache/_Nette.FileTemplate/_Volby.cron.latte-4f7177f280c5b25df186b8c790ac6b35.php
+++ _/temp/cache/_Nette.FileTemplate/_Volby.cron.latte-4f7177f280c5b25df186b8c790ac6b35.php
@@ -1,10 +1,10 @@
-<?php //netteCache[01]000378a:2:{s:4:"time";s:21:"0.08713600 1412722006";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:56:"D:\wamp\www\volbyonline\_\app\templates\Volby\cron.latte";i:2;i:1412722004;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:30:"b7f6732 released on 2013-01-01";}}}?><?php
+<?php //netteCache[01]000378a:2:{s:4:"time";s:21:"0.94400100 1382667469";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:56:"D:\wamp\www\volbyonline\_\app\templates\Volby\cron.latte";i:2;i:1370725342;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:30:"b7f6732 released on 2013-01-01";}}}?><?php
 
 // source file: D:\wamp\www\volbyonline\_\app\templates\Volby\cron.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '75bst7ggka')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'czbqtyod75')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 
@@ -18,4 +18,4 @@ if (!empty($_control->snippetMode)) {
 //
 ?>
 <h1>cron</h1>
-<p><?php echo $status ?></p>
+<p><?php echo Nette\Templating\Helpers::escapeHtml($status, ENT_NOQUOTES) ?></p>
